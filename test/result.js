b0d336af120d9fc15671fbd13fdb5a7162438586
zhouqian03 <zhouqian03@baidu.com>
2015-05-18 10:49:18 +0800

    update result

--- test/result.js
+++ test/result.js
@@ -1,23 +1,23 @@
 var dd = 3;
 var cc = 5;
-var mm = '10:18:16';
+var mm = '10:47:16';
 var kk = 11 + './origin.js';
 var lala = '2015-05-18';
-__C_EXTENSION('__DEBUG__', function () {
+(function () {
     console.log(123);
-});
-__C_EXTENSION('__METHOD__');
+}());;
+'31851b10-fd08-11e4-9b9a-2f5fa5b1f354';
 var a = function () {
-    var methodName = __C_EXTENSION('__METHOD__');
+    var methodName = 'a';
 };
 (function () {
-    var methodName = __C_EXTENSION('__METHOD__');
+    var methodName = '31851b11-fd08-11e4-9b9a-2f5fa5b1f354';
 }());
 a.bind('click', function () {
-    var methodName = __C_EXTENSION('__METHOD__');
+    var methodName = '31851b12-fd08-11e4-9b9a-2f5fa5b1f354';
 });
 var options = {
     callback: function () {
-        var methodName = __C_EXTENSION('__METHOD__');
+        var methodName = 'callback';
     }
 };
